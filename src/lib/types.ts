951fde51d0a312f537d3681458512354f844a24f
John Larence Lusaya <johnlarencelusaya@gmail.com>
2025-09-26 11:21:11 +0000

    during registration for admin or registration for invitation or any kind

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,15 @@
 
 import * as z from "zod";
 
+const passwordValidation = z
+  .string()
+  .min(8, { message: "Password must be at least 8 characters long." })
+  .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter." })
+  .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter." })
+  .regex(/[0-9]/, { message: "Password must contain at least one number." })
+  .regex(/[^A-Za-z0-9]/, { message: "Password must contain at least one special character." });
+
+
 export const loginSchema = z.object({
   email: z.string().email("Invalid email address."),
   password: z.string().min(6, "Password must be at least 6 characters."),
@@ -11,7 +20,7 @@ export const signUpSchema = z.object({
     domain: z.string().min(1, "Domain is required.").regex(/^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/, "Invalid domain format."),
     name: z.string().min(1, "Your name is required."),
     email: z.string().email("Invalid email address."),
-    password: z.string().min(6, "Password must be at least 6 characters."),
+    password: passwordValidation,
     confirmPassword: z.string()
 }).refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
@@ -23,7 +32,7 @@ export const signUpSchema = z.object({
 
 export const memberSignUpSchema = z.object({
     email: z.string().email("Invalid email address."),
-    password: z.string().min(6, "Password must be at least 6 characters."),
+    password: passwordValidation,
     confirmPassword: z.string()
 }).refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
