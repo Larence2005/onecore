a7ce5d69c1567beeb0700b2e5ddf4282225e7c5b
John Larence Lusaya <johnlarencelusaya@gmail.com>
2025-09-29 08:11:59 +0000

    make the username display name, and password all required field

--- src/components/settings-form.tsx
+++ src/components/settings-form.tsx
@@ -47,6 +47,7 @@ const passwordValidation = z
   .min(8, { message: "Password must be at least 8 characters long." })
   .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter." })
   .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter." })
+  .regex(/[0-9]/, { message: "Password must contain at least one number." })
   .regex(/[^A-Za-z0-9]/, { message: "Password must contain at least one special character." });
 
 const verificationFormSchema = z.object({
@@ -372,3 +373,5 @@ export function SettingsForm() {
     </div>
   );
 }
+
+    
