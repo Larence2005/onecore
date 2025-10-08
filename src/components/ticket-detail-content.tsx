9bd5c53132f2192ce2d8b90ea587a0ec0a89189f
John Larence Lusaya <johnlarencelusaya@gmail.com>
2025-10-08 04:51:11 +0000

    now apply that in ticket page

--- src/components/ticket-detail-content.tsx
+++ src/components/ticket-detail-content.tsx
@@ -1234,7 +1234,7 @@ return (
                     </div>
                 </Sidebar>
 
-                <main className="flex-1 flex flex-col min-w-0">
+                <main className="flex-1 flex flex-col min-w-0 bg-muted">
                      <Header>
                         <div className="flex items-center gap-4">
                             <Button variant="outline" size="icon" asChild>
