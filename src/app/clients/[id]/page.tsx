092c7524dfb634de3dcc466a99eff70fdb3d6b58
John Larence Lusaya <johnlarencelusaya@gmail.com>
2025-10-09 15:44:20 +0000

    Try fixing this error: `Runtime Error: Error: useSidebar must be used wi

--- src/app/clients/[id]/page.tsx
+++ src/app/clients/[id]/page.tsx
@@ -1,7 +1,12 @@
 // This is the Server Component page
 import { CompanyTicketsPageContent } from '@/components/company-tickets-view';
+import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function CompanyTicketsPage({ params }: { params: { id: string } }) {
   // Pass the ID as a prop to the Client Component
-  return <CompanyTicketsPageContent companyId={params.id} />;
+  return (
+    <SidebarProvider>
+      <CompanyTicketsPageContent companyId={params.id} />
+    </SidebarProvider>
+  );
 }
